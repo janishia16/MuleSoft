4d1fb9c65cf7f5720a9819c2f9224e36c4e84268
janishia16 <92728653+janishia16@users.noreply.github.com>
2022-06-17 14:45:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
     $password = "";                         
     $db = "movie";                              //database name
 
-    $conn = mysqli_connect($server,$user,$password,$db);    //connecting to database
+    $conn = mysqli_connect($server,$user,$password,$db);    //connection to database
 
     if(!$conn)                                //incase connection fails
     {
         echo "connection failed";       //exits the program
     }
-    $sql = "INSERT INTO movie (name , actor , actress , director , year ) VALUES ('ricky' , 'rakshit' , 'charlie' , 'shruthi' , 2003),('rocky' , 'yash' , 'nidhi' , 'neel' , 2022),('ASN' , 'ravi' , 'shreenidhi' , 'prashanth' , 2010),('vikram' , 'kamal' , 'pooja' , 'nelson' , 2022),('alone' , 'emran' , 'bipasha' , 'virappa' , 2001);       //query
+    $sql = "INSERT INTO movie (name , actor , actress , director , year ) VALUES ('Heaven' , 'Eric Roberts' , 'Marta Gil' , 'Angus Benfield' , 2020),('ABCD' , 'Prabhu Deva' , 'Lauren Gottlieb' , 'Remo D'Souza' , 2013),('dangal' , 'Aamir Khan' , 'Sakshi Tanwar' , 'Nitesh Tiwari' , 2016),('Dhoom 3' , 'abhishek' , 'katrina' , ' Vijay Krishna Acharya' , 2013),('The Terminator' , 'Michael Biehn' , 'Linda Hamilton' , 'James Cameron' , 1984);       //query
 
              $insert = mysqli_query($conn,$sql);                     //executing query
              echo '<script type="text/javascript">';
